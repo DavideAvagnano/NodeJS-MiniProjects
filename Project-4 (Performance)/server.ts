0f5748284fe28624b5062241a7dd9171e1eed277
DavideAvagnano <dav.avagnano@gmail.com>
2025-01-07 10:42:40 +0100

    Project-4 finish

--- Project-4 (Performance)/server.ts
+++ Project-4 (Performance)/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import cluster from "cluster";
-import os from "os";
+// import cluster from "cluster";
+// import os from "os";
 
 const delay = (duration: number) => {
   const startTime = Date.now();
@@ -28,28 +28,33 @@ app.get("/timer", (req: express.Request, res: express.Response) => {
   res.send(`Beep beep beep: ${process.pid}`);
 });
 
-//CLUSTER
-// First approach to improving node performance is using the built in "cluster module"
-console.log("Running server.ts");
-if (cluster.isPrimary) {
-  console.log("Primary has been started...");
-  /*
-   fork() function calls new worker processes
-   cluster.fork();
-   cluster.fork();
-   now when I make requests to /timer on chrome they are executed in parallel (but just 2 request in the same moment)
-  */
-  // use os to create the correct number of worker processes to run efficiently based on cpu logical cores
-  const NUM_WORKER = os.cpus().length;
-  for (let i = 0; i < NUM_WORKER; i++) {
-    cluster.fork();
-  }
-  // for my Macbook Air M1 (CPU 8-core) I created 8 workers
-  // it means in my browser I can make up to 8 requests at the same time and get increased performance
-} else {
-  console.log("Worker process started.");
-  app.listen(3000);
-}
+//CLUSTER (built in "cluster module")
+
+// console.log("Running server.ts");
+// if (cluster.isPrimary) {
+//   console.log("Primary has been started...");
+//   /*
+//    fork() function calls new worker processes
+//    cluster.fork();
+//    cluster.fork();
+//    now when I make requests to /timer on chrome they are executed in parallel (but just 2 request in the same moment)
+//   */
+//   // use os to create the correct number of worker processes to run efficiently based on cpu logical cores
+//   const NUM_WORKER = os.cpus().length;
+//   for (let i = 0; i < NUM_WORKER; i++) {
+//     cluster.fork();
+//   }
+//   // for my Macbook Air M1 (CPU 8-core) I created 8 workers
+//   // it means in my browser I can make up to 8 requests at the same time and get increased performance
+// } else {
+//   console.log("Worker process started.");
+//   app.listen(3000);
+// }
+
+// don't need to fork in ts because pm2 will fork the process
+console.log("Primary has been started...");
+console.log("Wroker process started.");
+app.listen(3000);
 
 // SERVER
 // const PORT = 8000;
